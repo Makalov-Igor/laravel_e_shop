da5165648ce15f1e0930c1db84a1371be6358261
Makalov Igor <IAMakalov@1cbit.ru>
2022-05-03 17:44:55 +0300

    Add layouts

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Sublime</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="Sublime project">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/bootstrap4/bootstrap.min.css') }}">
-    <link href="{{ asset('assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/plugins/OwlCarousel2-2.2.1/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/main_styles.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('assets/styles/responsive.css') }}">
-</head>
-<body>
+@extends('layouts.main')
+
+@section('content')
 
 <div class="super_container">
 
@@ -474,47 +460,4 @@
             </div>
         </div>
     </div>
-
-    <!-- Footer -->
-
-    <div class="footer_overlay"></div>
-    <footer class="footer">
-        <div class="footer_background" style="background-image:url({{ asset('assets/images/footer.jpg') }})"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center">
-                        <div class="footer_logo"><a href="#">Sublime.</a></div>
-                        <div class="copyright ml-auto mr-auto"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-                        <div class="footer_social ml-lg-auto">
-                            <ul>
-                                <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-</div>
-
-<script src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
-<script src="{{ asset('assets/styles/bootstrap4/popper.js') }}"></script>
-<script src="{{ asset('assets/styles/bootstrap4/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/greensock/TweenMax.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/greensock/TimelineMax.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/scrollmagic/ScrollMagic.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/greensock/animation.gsap.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/greensock/ScrollToPlugin.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/OwlCarousel2-2.2.1/owl.carousel.js') }}"></script>
-<script src="{{ asset('assets/plugins/Isotope/isotope.pkgd.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/easing/easing.js') }}"></script>
-<script src="{{ asset('assets/plugins/parallax-js-master/parallax.min.js') }}"></script>
-<script src="{{ asset('assets/js/custom.js') }}"></script>
-</body>
-</html>
+@endsection
